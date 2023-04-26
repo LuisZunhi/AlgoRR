ea3d7ce7a06b0fc9505ea2c012dbbad6a1da0cd7
Isra Mejia <43328345+IsraMejia@users.noreply.github.com>
2023-04-26 11:35:21 -0600

    Primer commit portada lista

---
+++ src/main/java/com/mycompany/soplanificadorrr/SOPlanificadorRR.java
@@ -0,0 +1,26 @@
+
+
+package com.mycompany.soplanificadorrr;
+
+/**
+ *
+ * @author IsraCode
+ */
+public class SOPlanificadorRR {
+
+    public static void main(String[] args) { 
+        presentacion();
+    }
+
+    public static void presentacion(){
+        System.out.println("\n\n");
+        System.out.println("******************************************");
+        System.out.println("          Facultad de Ingenieria          ");
+        System.out.println("Materia: Sistemas Operativos              ");
+        System.out.println("Profesora: Ing. Patricia Del Valle Morales");
+        System.out.println("Alumnos:");
+        System.out.println("Mejia Alba Israel Hipolito");
+
+    }
+}
+
